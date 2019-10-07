55a66a34875e57484d725ffe4752693ac1aa3be5
tekkabroley <alex.broley@gmail.com>
2019-10-06 21:12:24 -0700

    migrating DataGenerator project to new repo

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data-gen",
+    version="0.0.1",
+    author="Alex Broley",
+    author_email="alex.broley@gmail.com",
+    description="A package for generating random tabluar datasets. Designed to output to SQL, CSV or JSON.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tekkabroley/DataGenerator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
