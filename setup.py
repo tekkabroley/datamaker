366ff6f4265789bf2a5f3032e347c1e16b9d25c1
tekkabroley <alex.broley@gmail.com>
2019-10-18 14:21:23 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ datafiles = [
 
 setuptools.setup(
     name="datamaker-test-v2-alexbroley",
-    version="0.0.9",
+    version="0.1.0",
     author="Alex Broley",
     author_email="alex.broley@gmail.com",
     description="A package for generating tabluar datasets",
